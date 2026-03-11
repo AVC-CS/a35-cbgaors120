1992ae42dac8414d846a3927425da51fc5508c74
cbgaors120 <bgaor@students.avc.edu>
2026-03-10 18:03:28 -0700

    A35 Programming Challenge 5

--- main.cpp
+++ main.cpp
@@ -8,16 +8,25 @@ int main()
 {
     int rdnum1, rdnum2, rdnum3;
     int total = 0;
+    int avg;
 
     // TODO: seed the random number generator with time(0)
+    srand(time(0));
 
     // TODO: generate 3 random numbers between 0 and 99
+    
+    rdnum1 = rand() % 100; rdnum2 = rand() % 100; rdnum3 = rand() % 100;
 
     // TODO: compute the total summation
+    total = rdnum1 + rdnum2 + rdnum3;
 
     // TODO: print the 3 random numbers on the first line
-
+    avg = total / 3;
+    
     // TODO: print the total and average (integer division) on the second line
+    cout << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
+    cout << total << " " << avg << endl;
+
 
     return 0;
 }
